c444f5f17cdf8256420737ae673b73da22623995
hdyar392 <hannah.ghanie@hotmail.com>
2021-10-17 14:54:45 -0400

    <h1> new file 2nd attempt </h1>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Route, Link } from "react-router-dom";
+import {Route} from "react-router-dom";
 
 import HomePage from './Pages/aHomePage.js';
 import Guide from './Pages/bGuide.js';
 import Monitor_Control from './Pages/cMonitor-Control.js';
 import My_Aquarium from './Pages/dMy-Aquarium.js';
+import User_Manual from './Pages/eUser-Manual';
  
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/Guide" component={Guide}/>
         <Route exact path="/Monitor_Control" component={Monitor_Control}/>
         <Route exact path="/My_Aquarium" component={My_Aquarium}/>
+        <Route exact path="/User_Manual" component={User_Manual}/>
       </div>
   );
 }
